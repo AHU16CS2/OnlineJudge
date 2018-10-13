6f4391d60e044bb7a389b78e32b1b87e0327401e
Czzzzzy <861949338@qq.com>
2018-10-13 21:32:44 +0800

    Problist添加一条数据显示一条&能自动跳转到对应问题页面url&users已重新定义，迁移时数据库sqlite中似乎没有表，迁移文件未上传

--- problem/views.py
+++ problem/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 
 def problist(request):
-    return render(request, 'problem/problist.html')
+    problems = models.Problems.objects.all()
+    return render(request, 'problem/problist.html',{'problems':problems})
+
+
+def problem_page(request,Pid):
+    problem = models.Problems.objects.get(pk=Pid)
+    return render(request, 'problem/problem.html', {'problem': problem})
