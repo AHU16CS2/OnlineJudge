2221ff22f5750d79814af1ea5b27a8cc50fe7ac8
Yuxiang Chen <chenyx.cs@gmail.com>
2018-10-31 13:48:08 +0800

    完成了主页的分页、分类

--- oj_base/urls.py
+++ oj_base/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 
+
 app_name = 'oj_base'
 urlpatterns = [
     path('',views.yuming),
-    path('index/<int:list_id>', views.index,name='主页'),
-    path('newstype/<int:新闻种类_id>',views.news_with_type,name='新闻种类'),
+    path('index', views.index,name='主页'),
     path('news/<int:每个新闻_id>',views.news_detail, name='新闻'),
     path('news/edit/<int:每个新闻_id>',views.edit_new, name='编辑新闻'),
     path('about/', views.about, name='关于'),
